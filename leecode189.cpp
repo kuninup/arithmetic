19dc21d2017bf2c2f4fed793a4f0ab65fdef3f88
kuninup <kun0425@hunnu.edu.cn>
2019-12-19 12:27:54 +0800

    rm fs add 189

---
+++ leecode189.cpp
@@ -0,0 +1,51 @@
+class Solution1 {
+public:
+    void rotate(vector<int>& nums, int k) {
+       int previous;
+       for(int i=0;i<k;i++){
+           previous=nums[nums.size()-1];
+           for(int j=nums.size()-1;j>0;j--){
+               nums[j]=nums[j-1];
+           }
+           nums[0]=previous;
+       }
+    } 
+};
+
+class Solution2 {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        int count = 0;
+        for (int start = 0; count < nums.size(); start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.size();
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+};
+class Solution3 {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+    void reverse(vector<int>& nums,int start,int end){
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
